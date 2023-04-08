d09f501b3243490eb2d6f7319e4408709e537de2
SatwikMohan <satwikplay69@gmail.com>
2023-04-08 08:56:09 +0530

    ML Writing Api Added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+import 'BoardAPI.dart';
+
 void main() async{
   runApp(const MyApp());
   await Permission.storage;
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Board(),
+      home: BoardAPI(),
     );
   }
 }
